34239f768045aac72cb633fbb8a3610879e069e1
Dan Riding <doonyby@gmail.com>
2016-05-05 08:30:02 -0600

    assigned boxes

--- app.js
+++ app.js
@@ -8,15 +8,42 @@ $(document).ready(function() {
 		else {
 			$(this).text("X");
 		}
-		var gameTracker = $(this).text();
-		if (gameTracker === "O") {
-			console.log("O");
-		}
-		else {
-			console.log("X");
-		}
-
+	
+		evaluateBoard();
+		
 	});
 	
+	
+
+
+	function evaluateBoard(board) {
+		
+		var board = new Array(3);
+		for (var i = 0; i < board.length; i++) {
+		  board[i] = new Array(3);
+		}
+		
+		board[0][0] = $('#box1').text();
+		board[0][1] = $('#box2').text();
+		board[0][2] = $('#box3').text();
+		board[1][0] = $('#box4').text();
+		board[1][1] = $('#box5').text();
+		board[1][2] = $('#box6').text();
+		board[2][0] = $('#box7').text();
+		board[2][1] = $('#box8').text();
+		board[2][2] = $('#box9').text();
+
+		 for (var j = 0; j < 3; j++) {
+		    for (var k = 0; k < 3; k++) {
+
+		      if (board[0][0] && board[0][1] && board [0][2] === "X") {
+		      	console.log("X's win!")
+		      }
+		    }
+		  }
+
+		 
+	}	
 
 });
+
