145d8639aab3b60a87f16284d2cb16a134b388b8
Dan Riding <doonyby@gmail.com>
2016-05-05 09:21:24 -0600

    fixed grid

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 	$(".field").click(function() {
 		counter++;
 		if (counter % 2 === 0) {
-			$(this).text("O");
+			$(this).find('p').text("O");
 		}
 		else {
-			$(this).text("X");
+			$(this).find('p').text("X");
 		}
 	
 		evaluateBoard();
@@ -23,22 +23,26 @@ $(document).ready(function() {
 		  board[i] = new Array(3);
 		}
 		
-		board[0][0] = $('#box1').text();
-		board[0][1] = $('#box2').text();
-		board[0][2] = $('#box3').text();
-		board[1][0] = $('#box4').text();
-		board[1][1] = $('#box5').text();
-		board[1][2] = $('#box6').text();
-		board[2][0] = $('#box7').text();
-		board[2][1] = $('#box8').text();
-		board[2][2] = $('#box9').text();
-
+		board[0][0] = $('#box1 p').text();
+		board[0][1] = $('#box2 p').text();
+		board[0][2] = $('#box3 p').text();
+		board[1][0] = $('#box4 p').text();
+		board[1][1] = $('#box5 p').text();
+		board[1][2] = $('#box6 p').text();
+		board[2][0] = $('#box7 p').text();
+		board[2][1] = $('#box8 p').text();
+		board[2][2] = $('#box9 p').text();
+			
 		 for (var j = 0; j < 3; j++) {
+		    if (board[j][0] == board[j][1] && board[j][1] == board[j][2] && 
+		      	board[j][2] == board[j][0] && board[j][0] != " ") {
+		      	console.log(board[j][0] + "'s win! row");
+		    }
 		    for (var k = 0; k < 3; k++) {
-
-		      if (board[0][0] && board[0][1] && board [0][2] === "X") {
-		      	console.log("X's win!")
-		      }
+		    	if (board[j][k] == board[j][k] && board[j][k] == board[j][k] && 
+		      	board[j][k] == board[j][k] && board[j][k] != " ") {
+		      	console.log(board[j][k] + "'s win! column");
+			    }
 		    }
 		  }
 
