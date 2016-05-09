6efdff246555d6d945392bd2ceac4bfa5dcf6e96
Dan Riding <doonyby@gmail.com>
2016-05-09 16:42:08 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
 $(document).ready(function() {
-	var counter = 0;
-	$(".field").click(function() {
-		counter++;
-		if (counter % 2 === 0) {
-			$(this).find('p').text("O");
-		}
-		else {
-			$(this).find('p').text("X");
-		}
-	
-		evaluateBoard();
-		
-	});
-	
-	
 
 
-	function evaluateBoard(board) {
-		
-		var board = new Array(3);
-		for (var i = 0; i < board.length; i++) {
-		  board[i] = new Array(3);
-		}
-		
-		board[0][0] = $('#box1 p').text();
-		board[0][1] = $('#box2 p').text();
-		board[0][2] = $('#box3 p').text();
-		board[1][0] = $('#box4 p').text();
-		board[1][1] = $('#box5 p').text();
-		board[1][2] = $('#box6 p').text();
-		board[2][0] = $('#box7 p').text();
-		board[2][1] = $('#box8 p').text();
-		board[2][2] = $('#box9 p').text();
-			
-		 for (var j = 0; j < 3; j++) {
-		    if (board[j][0] == board[j][1] && board[j][1] == board[j][2] && 
-		      	board[j][2] == board[j][0] && board[j][0] != " ") {
-		      	console.log(board[j][0] + "'s win! row");
-		    }
-		    for (var k = 0; k < 3; k++) {
-		    	if (board[j][k] == board[j][k] && board[j][k] == board[j][k] && 
-		      	board[j][k] == board[j][k] && board[j][k] != " ") {
-		      	console.log(board[j][k] + "'s win! column");
-			    }
-		    }
-		  }
+ 
+var X = "X";
+var O = "O";
+var turn = X;
+var moves = 0;
 
-		 
-	}	
+function boardSet(Number) {
+	return document.getElementById("box" + Number).text();
+}
 
+function checkPattern(a, b, c, turn) {
+	var result = false;
+	if (boardSet(a) == turn && boardSet(b) == turn && boardSet(c) == turn) {
+		result = true;
+	};
+	return result;
+}
+
+function checkWin(turn) {
+	var result = false;
+	if (checkPattern(1, 2, 3, turn) || 
+		checkPattern(4, 5, 6, turn) ||
+		checkPattern(7, 8, 9, turn) || 
+		checkPattern(1, 4, 7, turn) ||
+		checkPattern(2, 5, 8, turn) ||
+		checkPattern(3, 6, 9, turn) ||
+		checkPattern(1, 5, 9, turn) ||
+		checkPattern(3, 5, 7, turn)) {
+
+		result = true;
+	} 
+	return result;
+}
+
+
+function setTurn() {
+		$('.field').click(function() {
+			if ($(this).text() == "") {	
+				$(this).text(turn);
+				moves++
+				console.log(moves);
+				switchTurn();
+			} else {
+				alert("This square has already been chosen.");
+			}
+		});
+};
+
+function switchTurn() {
+	if (turn === X) {
+		turn = O;
+	} else {
+		turn = X;
+	}
+	$('#gameTurn').text(turn + "'s Turn!")
+};
+
+boardSet();
+setTurn();	
 });
 
