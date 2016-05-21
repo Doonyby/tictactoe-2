6d070cbcbfa6bc508a6dac1de27d48eea5fa5239
Dan Riding <doonyby@gmail.com>
2016-05-20 21:29:11 -0600

    figured out box labels

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var X = "X";
 var O = "O";
 var turn = X;
 var counter = 0;
-
+for (var i = 1; i <= 9; i++) {
+		$('.grid-container').append('<div class="field" id="box' + i + '"></div>')
+	}
 
 
 
@@ -17,46 +19,51 @@ function setTurn() {
 			if ($(this).text() == "") {	
 				$(this).text(turn);
 				counter++
-				console.log(counter);
+				//console.log(counter);
 				var id = $(this).attr('id');
 				var choice = $(this).text();
-				checkWin();
+				
+				
+				
 				switchTurn();
+				//getChoice();
 				
 			} else {
 				alert("This square has already been chosen.");
 			}
-			function getChoice(number) {
-				return choice;	
+			function getChoice(box) {
+				//console.log(box);
+				//return choice;	
 			}
 			
-			function checkPattern(a, b, c, move) {
-				var result = false;
-				if (getChoice(a) == move && getChoice(b) == move && getChoice(c) == move) {
-					result = true;
-				};
+			// function checkPattern(a, b, c, move) {
+			// 	var result = false;
+			// 	if (getChoice(a) == move && getChoice(b) == move && getChoice(c) == move) {
+			// 		result = true;
+			// 	};
 				
-				console.log(result);
-				return result;
-			}
+			// 	checkWin();
+			// 	//console.log(result);
+			// 	return result;
+			// }
 
-			function checkWin(move) {
-				var result = false;
-				if (checkPattern(1, 2, 3, move) || 
-					checkPattern(4, 5, 6, move) ||
-					checkPattern(7, 8, 9, move) || 
-					checkPattern(1, 4, 7, move) ||
-					checkPattern(2, 5, 8, move) ||
-					checkPattern(3, 6, 9, move) ||
-					checkPattern(1, 5, 9, move) ||
-					checkPattern(3, 5, 7, move)) {
+			// function checkWin(move) {
+			// 	var result = false;
+			// 	if (checkPattern(1, 2, 3, move) || 
+			// 		checkPattern(4, 5, 6, move) ||
+			// 		checkPattern(7, 8, 9, move) || 
+			// 		checkPattern(1, 4, 7, move) ||
+			// 		checkPattern(2, 5, 8, move) ||
+			// 		checkPattern(3, 6, 9, move) ||
+			// 		checkPattern(1, 5, 9, move) ||
+			// 		checkPattern(3, 5, 7, move)) {
 
-					result = true;
-				} 
-				console.log(turn);
-				console.log(result);
-				return result;
-			}
+			// 		result = true;
+			// 	} 
+			// 	//console.log(turn);
+			// 	//console.log(result);
+			// 	return result;
+			// }
 
 		});	
 }
@@ -72,4 +79,6 @@ function switchTurn() {
 
 setTurn();	
 });
+
+
 
